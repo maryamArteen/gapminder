0f83f410b791a7bac0de680b217ed9d507cf7695
maryamArteen <s_arteen24@stud.hwr-berlin.de>
2025-06-16 12:23:15 +0200

    Initial Commit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("Hello BIPM!")
+st.title("Hello BIPM! :wave:")
+st.title("Hello BIPM! :wave: :smile:")
+st.title("Hello BIPM! :wave: :smile: :rocket:")
+st.title("Hello BIPM! :wave: :smile: :rocket: :tada:")
+st.title("Hello BIPM! :wave: :smile: :rocket: :tada: :sparkles:")
+st.title("Hello BIPM! :wave: :smile: :rocket: :tada: :sparkles: :heart:")
+st.title("Hello BIPM! :wave: :smile: :rocket: :tada: :sparkles: :heart: :star:")
+st.title("Hello BIPM! :wave: :smile: :rocket: :tada: :sparkles: :heart: :star: :fire:")
+st.title("")
